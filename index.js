9c696c6b6f1219974b6ce61ef9beda9977dd8e70
SHIVSB <2020kuec2005@iiitkota.ac.in>
2021-11-17 21:41:54 +0530

    All timer code

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+document.getElementById("submit").onclick = function(){
+    let target = document.getElementById("datetime").value;
+    let intervalId = setInterval(function(){
+        let current = new Date();
+        let difference = new Date(target).getTime() - current.getTime();
+        document.getElementById("days").innerText = Math.floor((difference) / (1000*60*60*24));
+        document.getElementById("hours").innerText = Math.floor(((difference) % (1000*60*60*24)) / (1000*60*60));
+        document.getElementById("minutes").innerText = Math.floor(((difference) % (1000*60*60)) / (1000*60));
+        document.getElementById("seconds").innerText = Math.floor(((difference) % (1000*60)) / (1000));
+        if(difference < 0){
+            clearInterval(intervalId);
+            document.getElementById("days").innerText = "NA";
+            document.getElementById("seconds").innerText = "NA";
+            document.getElementById("hours").innerText = "NA";
+            document.getElementById("minutes").innerText = "NA";
+        }
+    }, 1000);
+
+}
